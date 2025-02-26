9a8d45cb8a35f736c5c587f0c85748f1374a4c22
henrychris <64085210+henrychris@users.noreply.github.com>
2025-02-26 22:18:14 +0100

    pwa updates
    implement manifest and service worker
    
    add img assets
    
    move service worker to src/
    
    add system ui fonts
    
    link manifest with asset prefix
    
    detect sw updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,33 @@
 	import posthog from 'posthog-js';
 	import { browser } from '$app/environment';
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newWorker = registration.installing;
+			newWorker?.addEventListener('statechange', () => {
+				if (newWorker.state === 'installed') {
+					if (confirm('New update available! Reload to update?')) {
+						newWorker.postMessage({ type: 'SKIP_WAIT' });
+
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
 
 	if (browser) {
 		beforeNavigate(() => posthog.capture('$pageleave'));
 		afterNavigate(() => posthog.capture('$pageview'));
 	}
+
+	onMount(() => {
+		detectSWUpdate();
+	});
 
 	let { children } = $props();
 </script>
