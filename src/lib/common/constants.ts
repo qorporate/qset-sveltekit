d8557cfd07890dd74b1bdca427ddcf7558a2de3a
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2025-03-05 18:22:23 +0100

    fix timer falling asleep when phone screen is off (#2)
    also saves the timer progress if the page is refreshed. After refresh, the timer continues from where it stopped.

--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -1,5 +1,7 @@
 export const LOCAL_STORAGE_ITEM_GameState = 'GameState';
 export const LOCAL_STORAGE_ITEM_SelectedTime = 'selectedTime';
+export const LOCAL_STORAGE_ITEM_RemainingSeconds = 'timer_remaining_seconds';
+export const LOCAL_STORAGE_ITEM_EndTime = 'timer_end_time';
 
 export const EmailJs_PUBLIC_KEY = 'N9kf-ZYRoWAiH2kWq';
 export const EmailJs_SERVICE_ID = 'service_365k1gl';
