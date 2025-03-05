768076974341618114c9dd9b6730dac92d46437f
Ibidapo Fatogun <fatogunibidapo@gmail.com>
2025-03-05 11:11:55 +0100

    Revert "Background sync issue fix"
    This reverts commit f3069af75e12f7bf179fee3f65b565d3da15ebf7.

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/test-qset-sveltekit' : '',
+			base: '',
 			relative: false // to support postgres session replays
 		}
 	}
